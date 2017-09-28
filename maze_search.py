0909f79b1ba0e8f089e5d0fa7b6bb863ac2f503d
TheWinrawr <hujon1729@gmail.com>
2017-09-27 22:26:09 -0500

    init

---
+++ maze_search.py
@@ -0,0 +1,98 @@
+import queue as queue
+from utils import manhattan
+
+DIRS = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+def dfs(states, start, goal):
+	stack, num_expanded = [], 0;
+	visited = {} #the key is a coordinate, the value is the previous coordinate, this helps in constructing the final path
+	stack.append(start)
+	visited[start] = start
+
+	while stack:
+		coord = stack.pop()
+		if coord == goal:
+			return visited, num_expanded
+
+		expanded = False
+		for direction in DIRS:
+			nextCoord = (coord[0] + direction[0], coord[1] + direction[1])
+			if nextCoord in states and nextCoord not in visited:
+				stack.append(nextCoord)
+				visited[nextCoord] = coord
+				expanded = True
+
+		if expanded:
+			num_expanded += 1
+
+def bfs(states, start, goal):
+	q, num_expanded = queue.Queue(), 0
+	visited = {} #the key is a coordinate, the value is the previous coordinate, this helps in constructing the final path
+	q.put(start)
+	visited[start] = start
+	while q:
+		coord = q.get()
+		if coord == goal:
+			print("found goal")
+			return visited, num_expanded
+
+		expanded = False
+		for direction in DIRS:
+			nextCoord = (coord[0] + direction[0], coord[1] + direction[1])
+			if nextCoord in states and nextCoord not in visited:
+				q.put(nextCoord)
+				visited[nextCoord] = coord
+				expanded = True
+
+		if expanded:
+			num_expanded += 1
+
+def greedy(states, start, goal):
+	visited = {}
+	nodes, num_expanded = [], 0
+	start_node = (start, manhattan(start, goal))
+	nodes.append(start_node)
+	visited[start]  = start
+	while nodes:
+		min_node = min(nodes, key = lambda x:x[1])
+		coord = min_node[0]
+		nodes.remove(min_node)
+		if coord == goal:
+			print("found goal")
+			return visited, num_expanded
+
+		expanded = False
+		for direction in DIRS:
+			nextCoord = (coord[0] + direction[0], coord[1] + direction[1])
+			if nextCoord in states and nextCoord not in visited:
+				nodes.append((nextCoord, manhattan(nextCoord, goal)))
+				visited[nextCoord] = coord
+				expanded = True
+
+		if expanded:
+			num_expanded += 1
+
+def astar(states, start, goal):
+	visited = {}
+	nodes, num_expanded = [], 0
+	start_node = (start, manhattan(start, goal), 0)
+	nodes.append(start_node)
+	visited[start]  = start
+	while nodes:
+		min_node = min(nodes, key = lambda x:x[1])
+		coord, _, cost = min_node
+		nodes.remove(min_node)
+		if coord == goal:
+			print("found goal")
+			return visited, num_expanded
+
+		expanded = False
+		for direction in DIRS:
+			nextCoord = (coord[0] + direction[0], coord[1] + direction[1])
+			if nextCoord in states and nextCoord not in visited:
+				nodes.append((nextCoord, cost + manhattan(nextCoord, goal), cost+1))
+				visited[nextCoord] = coord
+				expanded = True
+
+		if expanded:
+			num_expanded += 1
---
+++ utils.py
@@ -0,0 +1,69 @@
+import copy
+
+PACMAN, WALL, DOT = 'P', '%', '.'
+
+def parse_file(file):
+	"""Parses a maze text file into useable data
+	
+	Arguments:
+		file {string} -- file name
+	
+	Returns:
+		list, set, tuple, list of tuple -- Returns the maze in list format, a set of possible states, the starting location (pacman), and a list of dot locations.
+	"""
+	maze = []
+	states = set()
+	pacman = (0,0)
+	dots = []
+
+	with open(file) as f:
+		lines = f.readlines()
+	lines = [line.strip() for line in lines]
+	
+	for x in range(len(lines)):
+		line = lines[x]
+		maze.append([])
+		for y in range(len(line)):
+			c = line[y]
+			maze[x].append(c)
+			if c == DOT:
+				dots.append((x, y))
+			if c == PACMAN:
+				pacman = (x,y)
+			if c != WALL:
+				states.add((x,y))
+	return maze, states, pacman, dots
+
+def manhattan(p1, p2):
+	"""Returns the manhattan distance between two points.
+	
+	Arguments:
+		p1 {tuple} -- first point
+		p2 {tuple} -- second point
+	
+	Returns:
+		int -- manhattan distance
+	"""
+	return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+
+def print_sol(output_path, maze, sol, num_nodes_expanded):
+	maze = copy.deepcopy(maze)
+	for c in sol:
+		maze[c[0]][c[1]] = '.'
+
+	with open(output_path, mode='w') as f:
+		for x in range(len(maze)):
+			for y in range(len(maze[0])):
+				f.write(maze[x][y])
+			f.write('\n')
+		f.write('Path length: ' + str(len(sol)) + '\n')
+		f.write('Nodes expanded: ' + str(num_nodes_expanded))
+
+def visited_to_path(visited, goal):
+	path = []
+	curr, prev = goal, visited[goal]
+	while curr != prev:
+		path.append(curr)
+		curr = prev
+		prev = visited[prev]
+	return path
